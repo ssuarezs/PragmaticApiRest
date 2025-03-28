c5f07a93651957b569244784640e1b2566aa468c
ssuarezs <suarez.santiago.113@gmail.com>
2025-03-28 10:33:56 -0500

    Setup Database using EF core

---
+++ DevHabit/DevHabit.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DevHabit.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevHabit.Api.Database;
+
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Habit> Habits { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(Schemas.Application);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
--- DevHabit/DevHabit.Api/Program.cs
+++ DevHabit/DevHabit.Api/Program.cs
@@ -1,3 +1,8 @@
+using DevHabit.Api.Database;
+using DevHabit.Api.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql;
 using OpenTelemetry;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
@@ -9,11 +14,20 @@ builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options
+        .UseNpgsql(
+            builder.Configuration.GetConnectionString("Database"),
+            ngsqlOptions => ngsqlOptions
+                .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
+        .UseSnakeCaseNamingConvention());
+
 builder.Services.AddOpenTelemetry()
     .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
     .WithTracing(tracing => tracing
         .AddHttpClientInstrumentation()
-        .AddAspNetCoreInstrumentation())
+        .AddAspNetCoreInstrumentation()
+        .AddNpgsql())
     .WithMetrics(metrics => metrics
         .AddHttpClientInstrumentation()
         .AddAspNetCoreInstrumentation()
@@ -31,6 +45,8 @@ WebApplication app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    await app.ApplyMigrationsAsync();
 }
 
 app.UseHttpsRedirection();
